307605c1812039d8329042c0a4ae336cbcef1c24
buildsample <buildsampletest@gmail.com>
2015-04-01 18:40:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -34,7 +34,7 @@ describe('Postgres Database', function () {
     db('things')
       .select()
       .then(function (docs) {
-        expect(docs[0].name).to.equal('Johnson');
+        expect(docs[0].name).to.equal('Johson');
         done();
       });
   });
